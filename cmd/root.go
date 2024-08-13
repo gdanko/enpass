4f6f0c50399208e49fe0b3b4782e1699e149263d
Gary Danko <gdanko@protonmail.com>
2024-08-13 16:18:37 -0700

    Change the flag short

--- cmd/root.go
+++ cmd/root.go
@@ -45,7 +45,7 @@ func Execute() error {
 func init() {
 	rootCmd.PersistentFlags().BoolVarP(&debug, "debug", "d", false, "Run enpass in debug mode")
 	rootCmd.PersistentFlags().StringVarP(&cardType, "type", "t", "password", "The type of your card. (password, ...)")
-	rootCmd.PersistentFlags().StringVarP(&vaultPath, "vault", "p", "", "Path to your Enpass vault")
+	rootCmd.PersistentFlags().StringVarP(&vaultPath, "vault", "v", "", "Path to your Enpass vault")
 	rootCmd.PersistentFlags().StringVarP(&keyFilePath, "keyfile", "k", "", "Path to your Enpass vault keyfile")
 	rootCmd.PersistentFlags().BoolVar(&nonInteractive, "nonInteractive", false, "Disable prompts and fail instead.")
 	rootCmd.PersistentFlags().StringVar(&defaultLogLevel, "log", "4", "The log level from debug (5) to panic (1)")
