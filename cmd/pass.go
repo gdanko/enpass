e72835b04649e3a5d7fb92fb58845c048e4947ba
Gary Danko <gdanko@protonmail.com>
2024-08-14 10:31:00 -0700

    Add relevant logic

--- cmd/pass.go
+++ cmd/pass.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/gdanko/enpass/util"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -22,11 +24,43 @@ func init() {
 }
 
 func passPreRunCmd(cmd *cobra.Command, args []string) error {
-	fmt.Println("passPreRun")
+	logger = logrus.New()
+
+	logLevel, err = logrus.ParseLevel(logLevelMap[logLevelStr])
+	if err != nil {
+		logrus.WithError(err).Fatal("invalid log level specified")
+	}
+	logger.SetLevel(logLevel)
+
+	if len(cmd.Flags().Args()) > 0 {
+		filters = cmd.Flags().Args()[0:]
+	} else {
+		filters = []string{}
+	}
+
 	return nil
 }
 
 func passRunCmd(cmd *cobra.Command, args []string) error {
-	fmt.Println("passRun")
+	vault, credentials, err = util.OpenVault(logger, pinEnable, nonInteractive, vaultPath, keyFilePath, logLevel)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		vault.Close()
+	}()
+	if err := vault.Open(credentials); err != nil {
+		logger.WithError(err).Error("could not open vault")
+		logger.Exit(2)
+	}
+	logger.Debug("opened vault")
+
+	card, err := vault.GetEntry(cardType, filters, true)
+	if err != nil {
+		return err
+	}
+	fmt.Println(card.DecryptedValue)
+
 	return nil
 }
