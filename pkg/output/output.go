2bca279211fb618aacb71f5e7597f4e04eb0be58
Gary Danko <gdanko@protonmail.com>
2024-08-14 10:34:19 -0700

    Remove unnecessary comments; add a loop to removd trashed items if we don't want to see them; add a loop to remove decrytped value for list operations

--- pkg/output/output.go
+++ pkg/output/output.go
@@ -16,41 +16,58 @@ import (
 
 func GenerateOutput(logger *logrus.Logger, cmdType string, flags *pflag.FlagSet, cards *[]enpass.Card) {
 	var (
-		err        error
-		jsonFlag   bool
-		listFlag   bool
-		yamlFlag   bool
-		yamlString string
+		err         error
+		jsonFlag    bool
+		listFlag    bool
+		trashedFlag bool
+		yamlFlag    bool
+		yamlString  string
 	)
 
-	// Remove decrypted if list
-	// Remove trashed if no trashed
-	for i, _ := range *cards {
-		if cmdType == "list" {
-			(*cards)[i].DecryptedValue = ""
-		}
-	}
-
-	// json
 	jsonFlag, err = flags.GetBool("json")
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 
-	// list
 	listFlag, err = flags.GetBool("list")
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 
-	// yaml
 	yamlFlag, err = flags.GetBool("yaml")
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
+
+	trashedFlag, err = flags.GetBool("trashed")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	// Loop through all of the cards and exclude trashed items unless we specify --trashed
+	cardsPruned := []enpass.Card{}
+	for _, cardItem := range *cards {
+		if cardItem.IsTrashed() {
+			if trashedFlag {
+				cardsPruned = append(cardsPruned, cardItem)
+			}
+		} else {
+			cardsPruned = append(cardsPruned, cardItem)
+		}
+	}
+
+	// If it's a list operation, DecryptedValue should be empty
+	for i := range cardsPruned {
+		if cmdType == "list" {
+			(cardsPruned)[i].DecryptedValue = ""
+		}
+	}
+
+	cards = &cardsPruned
 
 	if jsonFlag {
 		jsonBytes, _ := json.Marshal(cards)
@@ -96,7 +113,8 @@ func GenerateOutput(logger *logrus.Logger, cmdType string, flags *pflag.FlagSet,
 			row.Column(cardItem.Subtitle)
 			row.Column(cardItem.Category)
 			if cmdType == "show" {
-				row.Column(fmt.Sprint("%s: %s", cardItem.Type, cardItem.DecryptedValue))
+				password := fmt.Sprintf("%s: %s", cardItem.Type, cardItem.DecryptedValue)
+				row.Column(password)
 			}
 		}
 		tab.Print(os.Stdout)
