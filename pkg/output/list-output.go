22de04f7e270ac21e19229aa112534afb1b6b5fe
gdanko <Gary_Danko@intuit.com>
2024-08-27 12:42:17 -0700

    Display the decrypted value for list output format

--- pkg/output/list-output.go
+++ pkg/output/list-output.go
@@ -2,7 +2,6 @@ package output
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/fatih/color"
 	"github.com/gdanko/enpass/globals"
@@ -12,21 +11,21 @@ import (
 func doListOutput(cards []enpass.Card, cmdType string, nocolorFlag bool) {
 	for i, cardItem := range cards {
 		if nocolorFlag {
-			fmt.Printf("%s = %s\n", "      uuid", cardItem.UUID)
-			fmt.Printf("%s = %d\n", "   created", cardItem.CreatedAt)
-			fmt.Printf("%s = %s\n", " card_type", cardItem.Type)
-			fmt.Printf("%s = %s\n", "     title", cardItem.Title)
-			fmt.Printf("%s = %s\n", "     login", cardItem.Subtitle)
+			fmt.Printf("%s = %s\n", "           uuid", cardItem.UUID)
+			fmt.Printf("%s = %d\n", "        created", cardItem.CreatedAt)
+			fmt.Printf("%s = %s\n", "      card_type", cardItem.Type)
+			fmt.Printf("%s = %s\n", "          title", cardItem.Title)
+			fmt.Printf("%s = %s\n", "          login", cardItem.Subtitle)
 			if cardItem.Note != "" {
-				fmt.Printf("%s = %s\n", "      note", cardItem.Note)
+				fmt.Printf("%s = %s\n", "           note", cardItem.Note)
 			}
-			fmt.Printf("%s = %s\n", "  category", cardItem.Category)
-			fmt.Printf("%s = %s\n", "     label", cardItem.Label)
-			fmt.Printf("%s = %d\n", " last_used", cardItem.LastUsed)
-			fmt.Printf("%s = %v\n", " sensitive", cardItem.Sensitive)
-			fmt.Printf("%s = %v\n", "      icon", cardItem.Icon)
+			fmt.Printf("%s = %s\n", "       category", cardItem.Category)
+			fmt.Printf("%s = %s\n", "          label", cardItem.Label)
+			fmt.Printf("%s = %d\n", "      last_used", cardItem.LastUsed)
+			fmt.Printf("%s = %v\n", "      sensitive", cardItem.Sensitive)
+			fmt.Printf("%s = %v\n", "           icon", cardItem.Icon)
 			if cmdType == "show" {
-				fmt.Printf("%s = %s: %s\n", "type", cardItem.Type, cardItem.DecryptedValue)
+				fmt.Printf("%s = %s: %s\n", "decrypted_value", cardItem.Type, cardItem.DecryptedValue)
 			}
 		} else {
 			var (
@@ -35,23 +34,25 @@ func doListOutput(cards []enpass.Card, cmdType string, nocolorFlag bool) {
 				numberColor = color.New(colorMap[globals.GetConfig().Colors.NumberColor]).SprintFunc()
 				stringColor = color.New(colorMap[globals.GetConfig().Colors.StringColor]).SprintFunc()
 			)
-			fmt.Printf("%s = %s\n", keyColor("      uuid"), stringColor(cardItem.UUID))
-			fmt.Printf("%s = %s\n", keyColor("   created"), numberColor(cardItem.CreatedAt))
-			fmt.Printf("%s = %s\n", keyColor(" card_type"), stringColor(cardItem.Type))
-			fmt.Printf("%s = %s\n", keyColor("     title"), stringColor(cardItem.Title))
-			fmt.Printf("%s = %s\n", keyColor("  subtitle"), stringColor(cardItem.Subtitle))
+			fmt.Printf("%s = %s\n", keyColor("           uuid"), stringColor(cardItem.UUID))
+			fmt.Printf("%s = %s\n", keyColor("        created"), numberColor(cardItem.CreatedAt))
+			fmt.Printf("%s = %s\n", keyColor("      card_type"), stringColor(cardItem.Type))
+			fmt.Printf("%s = %s\n", keyColor("          title"), stringColor(cardItem.Title))
+			fmt.Printf("%s = %s\n", keyColor("       subtitle"), stringColor(cardItem.Subtitle))
 			if cardItem.Note != "" {
-				fmt.Printf("%s = %s\n", keyColor("      note"), stringColor(cardItem.Note))
+				fmt.Printf("%s = %s\n", keyColor("           note"), stringColor(cardItem.Note))
 			}
-			fmt.Printf("%s = %s\n", keyColor("  category"), stringColor(cardItem.Category))
-			fmt.Printf("%s = %s\n", keyColor("     label"), stringColor(cardItem.Label))
-			fmt.Printf("%s = %s\n", keyColor(" last_used"), numberColor(cardItem.LastUsed))
-			fmt.Printf("%s = %s\n", keyColor(" sensitive"), boolColor(cardItem.Sensitive))
-			fmt.Printf("%s = %s\n", keyColor("      icon"), stringColor(cardItem.Icon))
+			fmt.Printf("%s = %s\n", keyColor("       category"), stringColor(cardItem.Category))
+			fmt.Printf("%s = %s\n", keyColor("          label"), stringColor(cardItem.Label))
+			fmt.Printf("%s = %s\n", keyColor("      last_used"), numberColor(cardItem.LastUsed))
+			fmt.Printf("%s = %s\n", keyColor("      sensitive"), boolColor(cardItem.Sensitive))
+			fmt.Printf("%s = %s\n", keyColor("           icon"), stringColor(cardItem.Icon))
+			if cmdType == "show" {
+				fmt.Printf("%s = %s\n", keyColor("decrypted_value"), stringColor(cardItem.DecryptedValue))
+			}
 		}
 		if i < len(cards)-1 {
 			fmt.Println()
 		}
 	}
-	os.Exit(0)
 }
